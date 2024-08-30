b51e853d2e4f8d4885d14fba11d693b680f72daa
yyasminputri <yasminputri3101@gmail.com>
2024-08-30 17:33:26 +0700

    Initial commit for folder satu

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Pertemuan2Controller;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Pertemuan1Controller;
+use App\Http\Controllers\Pertemuan3Controller;
+use App\Http\Middleware\AuthMiddleware;
+
+Route::get('/', function(){
+    return view('layout.base');
+});
+
+Route::prefix('/pertemuan1')->group(function(){
+ Route::match(['get', 'post'], '/genap-ganjil', [Pertemuan1Controller::class, 'genapGanjil'])->name('genap-ganjil');
+ Route::get('/fibbonaci',[Pertemuan1Controller::class,'fibonacci'])->name('fibonacci');
+ Route::get('/prima', [Pertemuan1Controller::class, 'bilanganPrima'])->name('bilangan-prima');
+ Route::get('/param', fn() => view('pertemuan1.param'))->name('param');
+
+ 
+ Route::get('/param/{param1}', [Pertemuan1Controller::class, 'param1'])->name('param1');
+ Route::get('/param/{param1}/{param2}', [Pertemuan1Controller::class, 'param2'])->name('param2');
+
+});
+
+
+Route::fallback(function () {
+    return redirect('/');
+});
